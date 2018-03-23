29beebbbe7ad8a19f3205905e7d3685b1ecbbe1e
Krokodyli <krokodyliad@gmail.com>
2018-03-23 14:26:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ fi
 cp bmngrBackup.sh $HOME/.bmngr/bmngrBackup.sh
 cp bmngrMonitor.sh $HOME/.bmngr/bmngrMonitor.sh
 cp bmngr.sh $HOME/.bmngr/bmngr.sh
-cp bmngr.service $HOME/.bmngr/bmngr.service
 touch $HOME/.bmngr/bmngrFiles
 mkdir $HOME/.bmngr/backups
 
@@ -25,5 +24,15 @@ echo -n "Add autostart (systemctl)? y/n "
 read ans
 if [ "$ans" == "y" ]
 then
+	echo "[Unit]" >> $HOME/.bmngr/bmngr.service
+	echo "Description=Backup Manager" >> $HOME/.bmngr/bmngr.service
+	echo "" >> $HOME/.bmngr/bmngr.service
+	echo "[Service]" >> $HOME/.bmngr/bmngr.service
+	echo "Type=oneshot" >> $HOME/.bmngr/bmngr.service
+	echo "ExecStart=$HOME/.bmngr/bmngr.sh start" >> $HOME/.bmngr/bmngr.service
+	echo "RemainAfterExit=true" >> $HOME/.bmngr/bmngr.service
+	echo "" >> $HOME/.bmngr/bmngr.service
+	echo "[Install]" >> $HOME/.bmngr/bmngr.service
+	echo "WantedBy=default.target" >> $HOME/.bmngr/bmngr.service
 	systemctl --user enable --now $HOME/.bmngr/bmngr.service
 fi
